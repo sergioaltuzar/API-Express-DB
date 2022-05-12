be1c98255321493164155d93743becec80d15370
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-05-12 11:47:36 -0500

    agregando post y put

--- server.js
+++ server.js
@@ -19,3 +19,35 @@ app.get('/explorers', async (req, res) => {
     const allExplorers =  await prisma.explorer.findMany({});
     res.json(allExplorers);
   });
+
+  app.get('/explorers/:id', async (req, res) => {
+    const id = req.params.id;
+    const explorer = await prisma.explorer.findUnique({where: {id: parseInt(id)}});
+    res.json(explorer);
+  });
+
+  app.post('/explorers', async (req, res) => {
+    const explorer = {
+      name: req.body.name,
+      username: req.body.username,
+      mission: req.body.mission
+     };
+    const message = 'Explorer creado.';
+    await prisma.explorer.create({data: explorer});
+    return res.json({message});
+  });
+
+  app.put('/explorers/:id', async (req, res) => {
+	const id = parseInt(req.params.id);
+
+	await prisma.explorer.update({
+		where: {
+			id: id
+		},
+		data: {
+			mission: req.body.mission
+		}
+	})
+
+	return res.json({message: "Actualizado correctamente"});
+});
